38db690c39f3d6caa643242afa52e870e2824af1
nguu0123 <nguu0123@gmail.com>
2024-03-22 13:12:11 +0200

    fix conflict

--- qoa4ml_lib/qoa4ml/datamodels/common_models.py
+++ qoa4ml_lib/qoa4ml/datamodels/common_models.py
@@ -3,7 +3,9 @@ from pydantic import BaseModel
 import datamodel_enum
 
 
+
 class Metric(BaseModel):
+<<<<<<< HEAD
     metric_name: Union[
         datamodel_enum.ServiceMetricNameEnum, datamodel_enum.MlSpecificMetricNameEnum
     ]
@@ -14,12 +16,23 @@ class Metric(BaseModel):
 class Condition(BaseModel):
     operator: datamodel_enum.OperatorEnum
     value: Union[dict, float, int]
+=======
+    metric_name: ServiceMetricNameEnum | MlSpecificMetricNameEnum | str
+    records: List[dict | float | int]
+    unit: Optional[str]
+
+
+class Condition(BaseModel):
+    operator: OperatorEnum
+    value: dict | float | int
+>>>>>>> c0719ca (update)
 
 
 class MetricConstraint(BaseModel):
     metrics: Metric
     condition: Condition
     aggregate_function: datamodel_enum.AggregateFunctionEnum
+
 
 
 class BaseConstraint(BaseModel):
