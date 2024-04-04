06f6fbde5b7d63cfd133d8064b09774572c5eace
nguu0123 <nguu0123@gmail.com>
2024-04-04 15:32:16 +0300

    fix merge

--- qoa4ml_lib/qoa4ml/datamodels/common_models.py
+++ qoa4ml_lib/qoa4ml/datamodels/common_models.py
@@ -4,24 +4,45 @@ from typing import Dict, List, Optional, Union
 from .datamodel_enum import *
 
 
+
 class Metric(BaseModel):
+<<<<<<< HEAD
     metric_name: MetricNameEnum
     unit: Optional[str]
 
 
 class MetricRecord(Metric):
     records: List[Union[dict, float, int]]
+=======
+<<<<<<< HEAD
+    metric_name: Union[
+        datamodel_enum.ServiceMetricNameEnum, datamodel_enum.MlSpecificMetricNameEnum
+    ]
+    records: List[Union[dict, float, int]] = []
+    unit: Optional[str] = None
+>>>>>>> 6b135f6 (fix conflict)
 
 
 class Condition(BaseModel):
     operator: OperatorEnum
     value: Union[dict, float, int]
+=======
+    metric_name: ServiceMetricNameEnum | MlSpecificMetricNameEnum | str
+    records: List[dict | float | int]
+    unit: Optional[str]
+
+
+class Condition(BaseModel):
+    operator: OperatorEnum
+    value: dict | float | int
+>>>>>>> c0719ca (update)
 
 
 class MetricConstraint(BaseModel):
     metrics: Metric
     condition: Condition
     aggregate_function: AggregateFunctionEnum
+
 
 
 class BaseConstraint(BaseModel):
