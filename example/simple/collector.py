194e0ae658f99c5b4141d10d22cfe49bbe76043d
Tri Nguyen <tri.m.nguyen@aalto.fi>
2024-05-27 14:25:45 +0300

    update example

---
+++ example/simple/collector.py
@@ -0,0 +1,23 @@
+import argparse
+import os
+import sys
+
+from qoa4ml.collector.amqp_collector import Amqp_Collector
+from qoa4ml.config.configs import AMQPCollectorConfig
+
+conf = {
+    "end_point": "195.148.22.62",
+    "exchange_name": "test_qoa4ml",
+    "exchange_type": "topic",
+    "in_routing_key": "test.#",
+    "in_queue": "collector_1",
+}
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Node Monitoring")
+    parser.add_argument("--conf", help="configuration file", default="collector.json")
+    args = parser.parse_args()
+    ampq_conf = AMQPCollectorConfig(**conf)
+    collector = Amqp_Collector(configuration=ampq_conf)
+    collector.start_collecting()
