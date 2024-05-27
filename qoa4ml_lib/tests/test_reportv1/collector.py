866c2060286efd8f894e8870b82227a62f8505a6
nguu0123 <nguu0123@gmail.com>
2024-05-27 10:28:48 +0300

    add example test for ensemble ML

---
+++ qoa4ml_lib/tests/test_reportv1/collector.py
@@ -0,0 +1,22 @@
+import argparse
+import os
+import sys
+
+from qoa4ml.collector.amqp_collector import Amqp_Collector
+from qoa4ml.datamodels.configs import AMQPCollectorConfig
+
+conf = {
+    "end_point": "localhost",
+    "exchange_name": "test_qoa4ml",
+    "exchange_type": "topic",
+    "in_routing_key": "test.#",
+    "in_queue": "collector_1",
+}
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Node Monitoring")
+    parser.add_argument("--conf", help="configuration file", default="collector.json")
+    args = parser.parse_args()
+    ampq_conf = AMQPCollectorConfig(**conf)
+    collector = Amqp_Collector(configuration=ampq_conf)
+    collector.start_collecting()
