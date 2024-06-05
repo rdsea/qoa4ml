93df801e03653da0f4224f1b3bf887577e58866d
nguu0123 <nguu0123@gmail.com>
2024-06-05 10:11:28 +0300

    small update

--- qoa4ml_lib/setup.py
+++ qoa4ml_lib/setup.py
@@ -1,5 +1,8 @@
 from setuptools import find_packages, setup
 
+ml_requirements = [line.strip() for line in open("ml_requirements.txt").readlines()]
+dev_requirements = [line.strip() for line in open("dev_requirements.txt").readlines()]
+all = ml_requirements + dev_requirements
 setup(
     name="qoa4ml",
     version=open("VERSION").read(),
@@ -12,10 +15,7 @@ setup(
     email="tri.m.nguyen@aalto.fi",
     keyword="Monitoring Machine Learning",
     install_requires=[line.strip() for line in open("requirements.txt").readlines()],
-    extras_require={
-        "ml": [line.strip() for line in open("ml_requirements.txt").readlines()],
-        "dev": [line.strip() for line in open("dev_requirements.txt").readlines()],
-    },
+    extras_require={"ml": ml_requirements, "dev": dev_requirements, "all": all},
     packages=find_packages(),
     license="Apache License 2.0",
 )
