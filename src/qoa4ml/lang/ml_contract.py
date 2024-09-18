c45a52b6dc729525fbde5ff13398cc15fe0fdc18
nguu0123 <nguu0123@gmail.com>
2024-09-18 11:50:47 +0300

    refactor qoa4ml-core

--- src/qoa4ml/lang/ml_contract.py
+++ src/qoa4ml/lang/ml_contract.py
@@ -15,8 +15,8 @@ from .datamodel_enum import (
     StakeholderRoleEnum,
 )
 
-ML_CONTRACT_VERSION = "v0.1"
-ML_CONTRACT_NAME = "qoa4ml-ml-contract-schema"
+ML_CONTRACT_VERSION = "v0.3"
+ML_CONTRACT_NAME = "qoa4ml-contract-schema"
 
 
 class Stakeholder(BaseModel):
